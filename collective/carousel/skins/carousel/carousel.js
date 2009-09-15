97f828e5b4344ce7ae09274d7f082c6403e6f402
Denys Mishunov <denys@jarn.com>
2009-09-15 14:14:01 +0000

    fixed height for #carousel to wrap items based on their maximum height

--- collective/carousel/skins/carousel/carousel.js
+++ collective/carousel/skins/carousel/carousel.js
@@ -1,5 +1,13 @@
 // What is $(document).ready ? See: http://flowplayer.org/tools/using.html#document_ready
 jQuery(function($) {
+    // adjust height of the carousel to the max height of the elements
+    base_height = Math.max.apply(null,
+        $("#flowpanes .items .tileItem").map(function() { return $(this).innerHeight() }).get()
+    );
+    $("#flowpanes .items .tileItem").height(base_height);
+    $("#flowpanes").height(base_height);    
+    $("#carousel").height($("#flowpanes .items .tileItem").outerHeight(true) + $(".navi").outerHeight(true) + 10);
+        
     // initialize scrollable 
     $("div.scrollable").scrollable({
         size: 1,        
