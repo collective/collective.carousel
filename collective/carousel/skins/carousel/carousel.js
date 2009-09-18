2a29b2d8b200c98b4cd5540606537d7d64c4d451
Denys Mishunov <denys@jarn.com>
2009-09-18 12:52:58 +0000

    fixed issues with height of the carousel and it being stuck in between 2 items when page is Shift-reloaded

--- collective/carousel/skins/carousel/carousel.js
+++ collective/carousel/skins/carousel/carousel.js
@@ -1,17 +1,27 @@
 // What is $(document).ready ? See: http://flowplayer.org/tools/using.html#document_ready
 jQuery(function($) {
-    // adjust height of the carousel to the max height of the elements
-    base_height = Math.max.apply(null,
-        $("#flowpanes .items .tileItem").map(function() { return $(this).innerHeight() }).get()
-    );
-    $("#flowpanes .items .tileItem").height(base_height);
-    $("#flowpanes").height(base_height);    
-    $("#carousel").height($("#flowpanes .items .tileItem").outerHeight(true) + $(".navi").outerHeight(true) + 10);
-    // 35px in the following like is 20px (height of navi) + 15px (1/2 height of the button)
-    $(".browse").css("margin-top", ($("#carousel").height()/2 - 35));
-        
+    var elems = $('#flowpanes .items .tileItem');
+
+    var resizeCarousel = function() {
+        // adjust height of the carousel to the max height of the elements
+        var base_height = Math.max.apply(null,
+            elems.map(function() { return $(this).innerHeight() }).get()
+        );
+        elems.height(base_height);
+        $("#flowpanes").height(base_height);    
+        $("#carousel").height(elems.outerHeight(true) + $(".navi").outerHeight(true) + 10);
+        // 35px in the following like is 20px (height of navi) + 15px (1/2 height of the button)
+        $(".browse").css("margin-top", ($("#carousel").height()/2 - 35));
+    };
+    elems.find("img").load(function() {
+        resizeCarousel(); 
+    });
+
     // initialize scrollable 
     $("div.scrollable").scrollable({
-        size: 1,        
+        size: 1,  
+        clickable: false,      
+        loop: true,
     }).circular().autoscroll(25000).navigator();
+ 
 })
