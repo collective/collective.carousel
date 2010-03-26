9687fdddfc4ad746c4dbe4714ad55237f7a6837b
Denys Mishunov <denys@jarn.com>
2010-03-26 15:36:38 +0000

    some re-factoring of CSS, JS. Plus re-ordering some elements

--- collective/carousel/skins/carousel/carousel.js
+++ collective/carousel/skins/carousel/carousel.js
@@ -3,12 +3,8 @@ jQuery(function($) {
     var carousels = $(".carousel");
 
     var resizeCarousel = function(carousel, elems) {
-        // set width of all elems so they wrap and have correct heights
-        var cwidth = $(carousel).find(".scrollable").eq(0).innerWidth();
-        for (i=0; i<elems.length; i++) {   
-            $(elems[i]).css( {width: cwidth } );
-        };
-        
+        var scrollable = $(carousel).find(".scrollable").eq(0);
+                
         // adjust height of the carousel to the max height of the elements
         var base_height = Math.max.apply(null,
             elems.map(function() { return $(this).innerHeight() }).get()
@@ -16,24 +12,41 @@ jQuery(function($) {
         
         elems.height(base_height);
         
-        $(carousel).find(".scrollable").height(base_height);    
+        $(scrollable).height(base_height);    
         $(carousel).height(elems.outerHeight(true) + $(".navi").outerHeight(true) + 10);
         // 35px in the following like is 20px (height of navi) + 15px (1/2 height of the button)
         $(carousel).find(".browse").css("margin-top", (($(carousel).height()/2) - 20 - 15));
     };
     
+    carousels.each( function(i) {
+        var scrollable = $(this).find(".scrollable").eq(0);        
+        var elems = $(scrollable).find('.tileItem');
+        // set width of all elems so they wrap and have correct heights
+        var cwidth = $(this).innerWidth();
+        
+        $(scrollable).width(cwidth);
+        
+        for (i=0; i<elems.length; i++) {   
+            $(elems[i]).css( {width: cwidth } );
+        };
+
+        if($(scrollable).find("img").length == 0) {
+            // We resize right away
+            resizeCarousel($(this), elems);             
+        }
+        else {
+            // We wait until all images are loaded and resize afterwards
+            $(scrollable).find("img").load(function() {
+                c = carousels[i];
+                alert();
+                resizeCarousel($(this), elems);          
+            });            
+        }
+    })    
+    
     // doesn't make sense to enable autoscrolling if more than one 
     // carousel is on a page - this just distracts and annoys
-    var ap = (carousels.length == 1) ? true : false;
-    
-    carousels.each( function(i) {
-        var elems = $(this).find('.scrollable .items .tileItem');
-        resizeCarousel($(this), elems); 
-        elems.find("img").load(function() {
-            c = carousels[i];
-            resizeCarousel(c, elems); 
-        });
-    })    
+    var ap = (carousels.length == 1) ? true : false;  
     
     // initialize scrollable 
     var api = $("div.scrollable").scrollable({
@@ -42,4 +55,5 @@ jQuery(function($) {
         loop: true
     }).circular().autoscroll({autoplay: ap,steps:1,interval:25000}).navigator({api:true});
       
+    // Show navigation arrows and toolBar when hovering over a carousel
 })
