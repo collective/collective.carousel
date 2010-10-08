037894c928159c25f915d79af00fcc59d97a375f
djay <djay>
2010-10-08 14:46:40 +0000

    enable hiding borders and controls of carousel portlet

--- collective/carousel/portlets/carousel.py
+++ collective/carousel/portlets/carousel.py
@@ -1,11 +1,14 @@
 from zope.component import queryMultiAdapter
 from zope.i18nmessageid import MessageFactory
 from zope.interface import implements
+from zope import schema
+from zope.formlib import form
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 
 from plone.portlet.collection import collection as base
+from plone.app.form.widgets.uberselectionwidget import UberSelectionWidget
 
 _ = MessageFactory('collective.carousel')
 
@@ -13,10 +16,44 @@ _ = MessageFactory('collective.carousel')
 class ICarouselPortlet(base.ICollectionPortlet):
     """A portlet displaying a carousel with a Collection's results
     """
+    omit_border = schema.Bool(
+        title=_(u"Omit portlet border"),
+        description=_(u"Tick this box if you want to render the text above "
+                      "without the standard header, border or footer."),
+        required=True,
+        default=False)
+
+    hide_controls = schema.Bool(
+        title=_(u"Hide controls"),
+        description=_(u"Tick this box if you want to temporarily hide "
+                      "carousel controls i.e. next, prev, pause buttons."),
+        required=True,
+        default=False)
 
 class Assignment(base.Assignment):
     implements(ICarouselPortlet)
     
+    header = u""
+    target_collection=None
+    limit = None
+    random = False
+    show_more = True
+    show_dates = False
+    omit_border = False
+    hide_controls = False
+
+    def __init__(self, header=u"", target_collection=None, limit=None,
+                 random=False, show_more=True, show_dates=False,
+                 omit_border=False, hide_controls=False):
+        super(Assignment, self).__init__(header=header,
+                                         target_collection=target_collection, 
+                                         limit=limit,
+                                         random=random,
+                                         show_more=show_more,
+                                         show_dates=show_dates)
+        self.omit_border = omit_border
+        self.hide_controls = hide_controls
+
     @property
     def title(self):
         """This property is used to give the title of the portlet in the
@@ -44,6 +81,9 @@ class Renderer(base.Renderer):
         return tile()
 
 class AddForm(base.AddForm):
+    form_fields = form.Fields(ICarouselPortlet)
+    form_fields['target_collection'].custom_widget = UberSelectionWidget
+
     label = _(u"Add Carousel Portlet")
     description = _(u"This portlet display a listing of items from a Collection as a carousel.")
     
@@ -51,5 +91,8 @@ class AddForm(base.AddForm):
         return Assignment(**data)
 
 class EditForm(base.EditForm):
+    form_fields = form.Fields(ICarouselPortlet)
+    form_fields['target_collection'].custom_widget = UberSelectionWidget
+
     label = _(u"Edit Carousel Portlet")
     description = _(u"This portlet display a listing of items from a Collection as a carousel.")
