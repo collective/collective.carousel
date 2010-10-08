7ae90cae79ee10264af1a8f5fa111441eff5b0ac
djay <djay>
2010-10-08 14:48:55 +0000

    moved leadimage code back into c.carousel

--- collective/carousel/interfaces.py
+++ collective/carousel/interfaces.py
@@ -9,3 +9,15 @@ class ICarouselProvider(Interface):
     """ We should be able to mark objects that can be used in carousel with 
         the special marker. For now this marks Collections only.
     """
+
+try:
+    from collective.contentleadimage.interfaces import ILeadImageable
+except:
+    ILeadImageable = None
+from Products.ATContentTypes.interface import IATContentType
+from Products.ATContentTypes.interface import IATNewsItem
+
+if ILeadImageable is not None:
+    # Need to make sure we come up top
+    class IATWithLeadImage(ILeadImageable, IATContentType):
+        """ More specific interface """
