790f34d6b9d2aac19055b26b4c9626517079fdab
Johannes Raggam <raggam-nl@adm.at>
2013-09-16 15:23:59 +0200

    pep8

--- collective/carousel/vocabularies.py
+++ collective/carousel/vocabularies.py
@@ -7,7 +7,7 @@ from plone.app.imaging.utils import getAllowedSizes
 def ImageScaleVocabulary(context):
     allowed_sizes = getAllowedSizes()
     items = [(u'%s(%s, %s)' % (key, value[0], value[1]), key)
-            for key, value in allowed_sizes.items() if allowed_sizes]
+             for key, value in allowed_sizes.items() if allowed_sizes]
     return SimpleVocabulary.fromItems(items)
 
 directlyProvides(ImageScaleVocabulary, IVocabularyFactory)
